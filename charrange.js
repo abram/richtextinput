f9be866f0fd41ed92691098eab9f54bed590f626
Abe Fettig <abe@fettig.net>
2010-06-13 16:37:53 -0400

    Add support for IE

--- charrange.js
+++ charrange.js
@@ -5,7 +5,7 @@ richtext.CharRange = function(containerElement) {
 }
 
 richtext.CharRange.prototype.refresh = function() {
-  this.nodes.length = [];
+  this.nodes = [];
   this.buildNodeList_(this.containerElement, this.nodes);
 
   // TODO: remove this hackery.
@@ -42,39 +42,69 @@ richtext.CharRange.prototype.isInContainer_ = function(child) {
     return false;
 };
 
-richtext.CharRange.prototype.containsRange_ = function(range) {
-    return this.isInContainer_(range.startContainer) &&
-        this.isInContainer_(range.endContainer);
-};
 
 richtext.CharRange.prototype.getSelectionIndexes = function() { 
+  if (document.selection) {
+    var indices = this.getSelectionIndexesIE_();
+  } else {
+    var indices = this.getSelectionIndexesW3C_();
+  }
+  return indices || [null, null];
+};
+
+richtext.CharRange.prototype.getSelectionIndexesIE_ = function() {
+  var textRange = document.selection.createRange();
+  if (!this.isInContainer_(textRange.parentElement)) {
+    return null;
+  }
+  var startRange = textRange.duplicate();
+  //var endRange = textRange.duplicate();
+  startRange.collapse(true);
+  var startNode = startRange.parentElement;
+  var startNodeRange = textRange.duplicate();
+  startNodeRange.moveToElementText(startNode);
+  var entireNodeLength = startNodeRange.text.length;
+  startRange.setEndPoint('EndToStart', startNodeRange);
+  var startOffset = startRange.text.length - entireNodeLength;
+  var startIndex = this.getNodeStartIndex(startNode) + startOffset;
+  var endIndex = startIndex + textRange.text.length;
+  return [startIndex, endIndex];  
+};
+
+richtext.CharRange.prototype.getSelectionIndexesW3C_ = function() {
   var selection = window.getSelection();
   var range;
   if (selection.rangeCount) {
       range = selection.getRangeAt(0);
   }
-  if (!range || !this.containsRange_(range)) {
-    return [null, null];
+  if (!range || !this.containsRangeW3C_(range)) {
+    return null;
   }
-
-  var startContainerStartIndex = this.getNodeStartIndex(range.startContainer) || 0;
-  var endContainerStartIndex = this.getNodeStartIndex(range.endContainer) || 0;
+  var startContainerStartIndex =
+      this.getNodeStartIndex(range.startContainer) || 0;
+  var endContainerStartIndex =
+      this.getNodeStartIndex(range.endContainer) || 0;
   return [startContainerStartIndex + range.startOffset,
 	  endContainerStartIndex + range.endOffset];
+};
+
+richtext.CharRange.prototype.containsRangeW3C_ = function(range) {
+    return this.isInContainer_(range.startContainer) &&
+        this.isInContainer_(range.endContainer);
 };
 
 richtext.CharRange.prototype.setSelectionIndexes = function(startIndex,
 							    endIndex) {
   var startNode = this.nodes[startIndex];
   var endNode = this.nodes[endIndex];
-  var startOffset = this.getNodeStartIndex(startNode);
-  var endOffset = this.getNodeStartIndex(endNode);
-  richtext.log(startNode, startOffset, endNode, endOffset);
-  var range = document.createRange();
-  range.setStart(startNode, startIndex - startOffset);
-  range.setEnd(endNode, endIndex - endOffset);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(range);
+  var startOffset = startIndex - this.getNodeStartIndex(startNode);
+  var endOffset = endIndex - this.getNodeStartIndex(endNode);
+
+  if (document.selection) {
+    this.selectRangeIE_(startNode, startOffset, endNode, endOffset);
+  } else {
+    this.selectRangeW3C_(startNode, startOffset, endNode, endOffset);
+  }
 };
 
 richtext.CharRange.prototype.getNodeStartIndex = function(node) {
@@ -84,5 +114,33 @@ richtext.CharRange.prototype.getNodeStartIndex = function(node) {
       return i;
     }
   }
+};
+
+richtext.CharRange.prototype.selectRangeW3C_ = function(startNode, startOffset,
+							endNode, endOffset) {
+  var range = document.createRange();
+  range.setStart(startNode, startOffset)
+  range.setEnd(endNode, endOffset);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  
+};
+
+richtext.CharRange.prototype.selectRangeIE_ = function(startNode, startOffset,
+						       endNode, endOffset) {
+  var startRange = document.body.createTextRange();
+  var endRange = document.body.createTextRange();
+  startRange.moveToElementText(startNode.parentNode);
+  endRange.moveToElementText(endNode.parentNode);
+  if (startOffset) {
+    startRange.moveStart('character', startOffset);
+  }
+  if (endOffset) {
+    endRange.moveStart('character', endOffset);
+  }
+  var newRange = document.body.createTextRange();
+  newRange.setEndPoint('StartToStart', startRange);
+  newRange.setEndPoint('EndToStart', endRange);
+  newRange.select();
 };
 
